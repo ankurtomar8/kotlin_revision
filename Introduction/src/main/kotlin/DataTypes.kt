66b054145c32688fa9d70b1f108ac6831af56ee2
Dev-Ankur <ankurtomar8@outlook.com>
2025-11-22 10:13:19 +0530

    Koltin Revision   DataTypes

---
+++ Introduction/src/main/kotlin/DataTypes.kt
@@ -0,0 +1,17 @@
+package org.example
+
+class DataTypes {
+}
+
+fun main(){
+
+    var name: String = "Ankur"
+    var age : Int = 28
+    var marks: Float = 15.0f
+    var isStudent : Boolean = true
+    var newChar : Char = 'A'
+
+    println("$name $age $marks $isStudent $newChar")
+
+
+}
--- Introduction/src/main/kotlin/Main.kt
+++ Introduction/src/main/kotlin/Main.kt
@@ -4,7 +4,18 @@ fun main() {
 
     val name = "Ankur"
     println("$name Welcome to Kotlin")
-
     print("Hello World")
 
+    var number = 25;
+    // number ="Hi" // Error: Type mismatch Type Checking violation
+
+    var nameNew :String  = "Ankur"
+
+    println("$nameNew $number "  )
+
+    greetings("Good Morning ")
     }
+
+fun greetings( greet:String){
+
+}
