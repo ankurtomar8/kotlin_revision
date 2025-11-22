5e8da2a4e066021900dd7d4749b24ccd439e0165
Dev-Ankur <ankurtomar8@outlook.com>
2025-11-22 10:18:06 +0530

    Koltin Revision   DataTypes

---
+++ Introduction/src/main/kotlin/Operators.kt
@@ -0,0 +1,14 @@
+package org.example
+
+    fun main(){
+        val num1 = 10
+        val num2 = 20
+        // Addition
+        println("num1 + num2 = ${num1+num2}")
+        // Subtraction
+        println("num1 - num2 = ${num1 - num2}")
+        // Multiply
+        println("num1 * num2 = ${num1 *  num2}")
+        // Divide
+        println("num1 % num2 = ${num1 % num2 }")
+    }
