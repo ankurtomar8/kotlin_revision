ae2959896bf52175cfbe2db40f51372f05a2abf2
Dev-Ankur <ankurtomar8@outlook.com>
2025-12-19 09:03:41 +0530

    Revision2.kt added

--- Introduction/src/main/kotlin/Revision/Revison2.kt
+++ Introduction/src/main/kotlin/Revision/Revison2.kt
@@ -4,12 +4,28 @@ package org.example.Revision
 
 fun main(){
 
-    val age : Int = readln().toInt()
+    val age : Int = 18
     if( age in 18..30){
         println("Allowed to vote ")
     }else{
         println("Not allowed to vote")
     }
 
+    val timeofDay = "Night"
+
+    when(timeofDay){
+        "Morning" -> {println("Eat breakfast ")}
+        "Afternoon" -> {println(" Lunch Eat ")}
+        "Evening" -> {println("Evening snack ")}
+        "Night" -> {println("Dinner time")}
+        else -> {println("Vrat hai aaj ")}
+    }
+
+    var region : Int = 3
+    when(region){
+        1 -> {println("JPN is selected")}
+        2 -> {println("HKG is selected")}
+        3 -> {println("IND")}
+    }
 
 }
