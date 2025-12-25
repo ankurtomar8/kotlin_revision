e337e2c2d6e4e0235ec111ad6ddc67ebbc7870e4
Dev-Ankur <ankurtomar8@outlook.com>
2025-12-25 23:49:34 +0530

    InheritanceRevision.kt added

--- Introduction/src/main/kotlin/Revision/InheritanceRevision.kt
+++ Introduction/src/main/kotlin/Revision/InheritanceRevision.kt
@@ -10,7 +10,7 @@ open class InheritanceRevision {
 
 }
 
-open class  childClassHouse : InheritanceRevision(){
+open class  child1 : InheritanceRevision(){
     init {
         room = 3
         material = "Cement"
@@ -19,5 +19,31 @@ open class  childClassHouse : InheritanceRevision(){
     override fun describe() {
         println("childClassHouse")
     }
+
+}
+
+open class child2(val types: String) : child1(){
+    var type = types
+
+    init {
+        room = 5
+    }
+
+    override fun describe() {
+        super.describe()
+        println("Child Class Home is made of  $material and has $room")
+     }
+}
+
+fun main(){
+    //
+    val inheritanceClass = InheritanceRevision()
+    inheritanceClass.describe()
+
+    val Parent = child1()
+    Parent.describe()
+
+    val childClass = child2("Modern")
+    childClass.describe()
 
 }
